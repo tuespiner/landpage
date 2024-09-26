75d1cd2b4a842250f0c4177dd59e9a6a4617e700
tuespiner <98735870+tuespiner@users.noreply.github.com>
2024-09-26 20:23:07 +0200

    Add files via upload

--- script/js.js
+++ script/js.js
@@ -3,9 +3,9 @@ function cambiarIdioma(){
     var variable = document.getElementById("Texto");
     if(i == 0){
         i = 1;
-        variable.innerHTML = ' something will be shown here somewhen ';
+        variable.innerHTML = ' Something Will Spawn Here ';
     }else{
         i = 0;
-        variable.innerHTML = ' Algo Se Mostrara Aqui Tarde O Temprano ';
+        variable.innerHTML = ' Algo Spawneara Aqui Tarde O Temprano ';
     }
 }
